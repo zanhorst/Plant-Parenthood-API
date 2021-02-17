ba8393b45b6022806e225ec55f600eb8526e90ff
zanhorst <zanhorst@gmail.com>
2021-02-17 11:20:00 -0500

    second commit

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+ant_#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_auth_template.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plant_parenthood.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
