0aeb48ce57bd5efd3239d8fae9ebad38eade82fd
zanhorst <zanhorst@gmail.com>
2021-02-18 15:10:32 -0500

    Renamed all 'mango' keywords to post (including file names). Updated post.py from mango.py to include necessay keys. Added PostFilter to post_views and urls.py for users to be able to see only their own posts if they want.

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views.mango_views import Mangos, MangoDetail
+from .views.post_views import Posts, PostDetail, PostFilter
 from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
 
 urlpatterns = [
   	# Restful routing
-    path('mangos/', Mangos.as_view(), name='mangos'),
-    path('mangos/<int:pk>/', MangoDetail.as_view(), name='mango_detail'),
+    path('posts/', Posts.as_view(), name='post'),
+    path('posts/', PostFilter.as_view(), name='post'),
+    path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail'),
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
